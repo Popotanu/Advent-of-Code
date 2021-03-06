9c7d424f9abd0b4fb7fb27f621d52c667ffc71aa
popotanu <scrapmacaron@gmail.com>
2021-03-06 12:21:22 +0900

    standardrb --fix

--- 2020/day_01/answer.rb
+++ 2020/day_01/answer.rb
@@ -7,7 +7,7 @@
 
 OBJECTIVE = 2020
 numbers = []
-File.foreach('./input.txt') do |line|
+File.foreach("./input.txt") do |line|
   numbers << line.delete("\n").strip.to_i
 end
 
@@ -19,16 +19,16 @@ while numbers.length.positive?
   # as "numbers" has been sorted, "res1" is smaller than any other values in the array.
   break if res1 > OBJECTIVE / 2
   # using find-any mode
-  if res2 = numbers.bsearch{ |x| OBJECTIVE - (x + res1) }
+  if res2 = numbers.bsearch { |x| OBJECTIVE - (x + res1) }
     break
   end
 end
 
 if !res2
-  puts 'there arent any pair of numbers ><'
+  puts "there arent any pair of numbers ><"
   exit
 end
 
-puts 'found out!'
+puts "found out!"
 puts "the answer is (#{res1}, #{res2})"
 puts "and the result of multiply #{res1} and #{res2} is #{res1 * res2}"
